c8330f8c1bc5148cb3393a09fd62f990ce811833
helen <helen@hos67669.intranet.chuv>
2022-04-21 13:53:13 +0200

    Added global variables for replacing greek symbols

---
+++ R/globals.R
@@ -0,0 +1,32 @@
+# Rho?
+
+GREEKSYM_TO_NAME = c(α = "alpha", β = "beta", γ = "gamma", δ = "delta",
+                     ε = "epsilon", ζ = "zeta", κ = "kappa", λ = "lambda",
+                     τ = "tau")
+
+GREEKSYM_TO_LETTER =  c(α = "a", β = "b", γ = "g", δ = "d", ε = "e", ζ = "z",
+                         κ = "k", λ = "l", τ = "t")
+
+# Tau is not included, is it necessary?
+GREEKNAME_TO_LETTER = c(alpha = "a", beta = "b", gamma = "g", delta = "d",
+                        epsilon = "e", zeta = "z", kappa = "k", lambda = "l")
+
+
+#      η = "e", θ = "t", ι = "i", , μ = "m", ν = "n",
+#   ξ = "x", ο = "o", π = "p", ρ = "r", σ = "s", , υ = "u",
+#   φ = "p", χ = "c", ψ = "p", ω = "o", Α = "A", Β = "B", Γ = "G",
+#   Δ = "D", Ε = "E", Ζ = "Z", Η = "E", Θ = "T", Ι = "I", Κ = "K",
+#   Λ = "L", Μ = "M", Ν = "N", Ξ = "X", Ο = "O", Π = "P", Ρ = "R",
+#   Σ = "S", Τ = "T", Υ = "U", Φ = "P", Χ = "C", Ψ = "P", Ω = "O",
+#   )
+
+#eta theta  iota mu
+#"η" "θ" "ι" "μ"
+#nu   xi  omicron
+#"ν"  "ξ" "ο"
+#pi  rho sigma
+#"π"  "ρ" "σ"
+#upsilon  phi
+# υ" "φ"
+#chi  psi omega
+#"χ"  "ψ" "ω"
--- R/replaceGreekSyms.R
+++ R/replaceGreekSyms.R
@@ -1,3 +1,21 @@
-replaceGreekSyms <- function(){
+# replaceGreekSyms ----
+#'@title replaceGreekSyms
+#'@description Replace Greek symbols with either single letter or word.  Note
+#'that this only replaces greek symbols I have encountered in antibody names to
+#'avoid introducing errors by e.g. replacing "mu" in "immunoglobin".
+#'@param x Character vector containing Greek symbols to be replaced.
+#'@param replacement Either "sym2letter" (replace Greek symbols with single
+#'lowercase letter) or "sym2word" (replace Greek symbols with lowercase word,
+#'e.g. "alpha") or "word2letter" (replace lowercase Greek symbol names with
+#'single lowercase letters e.g. "alpha" to "a")
+replaceGreekSyms <- function(x,
+                        replace = c("sym2letter", "sym2word", "word2letter")){
+    # Select replacement vector
+    replace <- match.arg(replace)
+    replace_to_vec = c("sym2letter" = GREEKSYM_TO_LETTER,
+                       "sym2word" = GREEKSYM_TO_NAME,
+                       "word2letter" = GREEKNAME_TO_LETTER)
+    replace_vec <- replace_to_vec[replace]
+
 
 }
