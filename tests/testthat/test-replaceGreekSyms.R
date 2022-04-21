e60f716eef58cb0697ef1dea07575ac9b78c9312
helen <helen@hos67669.intranet.chuv>
2022-04-21 14:21:27 +0200

    replaceGreekLetter now works

--- tests/testthat/test-replaceGreekSyms.R
+++ tests/testthat/test-replaceGreekSyms.R
@@ -1,7 +1,8 @@
 test_that("replaceGreekSyms replaces Greek symbols", {
-    expect_equal(replaceGreekSyms("β2-microglobulin", replace = sym2word),
-                 "beta-2-microglobulin")
-    expect_equal(replaceGreekSyms("TCRγ/δ", replace = sym2letter), "TCRg/d")
-    expect_equal(replaceGreekSyms("TCR gamma/delta", replace = word2letter),
+    expect_equal(replaceGreekSyms("β2-microglobulin", replace = "sym2word"),
+                 "beta2-microglobulin")
+    expect_equal(replaceGreekSyms("TCRγ/δ", replace = "sym2letter"), "TCRg/d")
+    expect_equal(replaceGreekSyms("TCR gamma/delta", replace = "word2letter"),
                  "TCR g/d")
+    expect_equal(replaceGreekSyms("β2-microglobulin"), "b2-microglobulin")
 })
