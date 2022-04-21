6c12e6fe73dfa37a2ad8f10fd70abe393914c426
helen <helen@hos67669.intranet.chuv>
2022-04-21 16:50:11 +0200

    add some more templates

---
+++ R/searchHGNC.R
@@ -0,0 +1,9 @@
+# searchHGNC ----
+#'@title searchHGNC
+#'@description Search HGNC gene symbols, aliases and names for an exact match
+#'to an value, assumed to be an antigen name or part of an antigen name.
+#'@param gd A data.frame (or similar object, e.g. a tibble)
+searchHGNC <- function(qd){
+    data(hgnc_long)
+
+}
