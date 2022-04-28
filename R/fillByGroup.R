cfa6b88b842918d3cf930b79565db135eaa5ccee
helen <helen@hos67669.intranet.chuv>
2022-04-28 14:06:53 +0200

    working on fillByGroup

--- R/fillByGroup.R
+++ R/fillByGroup.R
@@ -3,6 +3,8 @@
 #'
 #'Wrapper of tidyr::fill with checks for NAs in grouping values and the option
 #'to fill with the majority value if there is more than one value per group.
+#'If rlang is available and an error is raised, an example group that causes an
+#'error is returned
 #'
 #'@param df A data.frame or tibble with missing (NA) values to be filled
 #'@param groups (character(n)) Names of column(s) to group by
@@ -17,21 +19,45 @@
 fillByGroup <- function(df, groups, fill, multiple = c("stop", "majority")){
     multiple = match.arg(multiple)
     temp_names <- .tempColName(df, 2)
+    original_nrows <- nrow(df)
 
+    # Remove rows with NAs in group, merge after filling
     na_rows <- df %>%
         dplyr::filter(!complete.cases(across(all_of(groups))))
+
+    # Group data frame, check if there are multiple values per group
     df <- df %>%
         dplyr::filter(complete.cases(across(all_of(groups)))) %>%
         dplyr::group_by(!!!syms(groups)) %>%
         dplyr::mutate(!!temp_names[1] :=
                           dplyr::n_distinct(!!!syms(fill), na.rm = TRUE))
 
-    if (multiple == "stop" & any(df[,temp_names[1]] > 1)){
-        multi_df <- df %>% dplyr::filter(!!sym(temp_names[1]) > 1)
-        gp_rows <- multi_df %>% group_rows()
-
-        stop()
+    # Case: multiple possible values in a fill group and we should stop
+    if (multiple == "stop" & any(df[, temp_names[1]] > 1)){
+        msg <- "Some fill columns have multiple values per group"
+        if (requireNamespace("rlang")){
+            multi_df <- df %>% dplyr::filter(!!sym(temp_names[1]) > 1)
+            first_group <- .firstGroups(multi_df)
+            rlang::abort(message = msg, df = first_group)
+        } else {
+            stop(msg)
+        }
     }
 
+    # Case: only one value per group
+    if (all(df[, temp_names[1]] == 1)){
+        df <- tidyr::fill(df, !!!syms(fill), .direction = "updown") %>%
+            dplyr::ungroup()
+    }
 
+    # Case: multiple values per group, select the most frequent one
+    if (multiple == "majority"){
+
+    }
+
+    df <- full_join(df, na_rows)
+    if (! nrow(df) == original_nrows){
+        warning("Rows have been lost or gained when merging NA rows")
+    }
+    return(df)
 }
--- tests/testthat/test-fillByGroup.R
+++ tests/testthat/test-fillByGroup.R
@@ -1,6 +1,19 @@
-test_that("fillByGroup stops with multiple values", {
-    df <- data.frame(A = c(rep("A", 3), NA, rep("B", 4), rep("C", 3), NA),
-                     B = c(rep("A", 2), NA, "A", rep("B", 4), rep("C", 3), NA),
-                     C = c(NA, 1, 1, 1, 2, 3, NA, 2, 4, 4, NA, 5))
+df <- data.frame(A = c(rep("A", 3), NA, rep("B", 4), rep("C", 3), NA),
+                 B = c(rep("A", 2), NA, "A", rep("B", 4), rep("C", 3), NA),
+                 C = c(NA, 1, 1, 1, 2, 3, NA, 2, 4, 4, NA, 5))
+
+
+test_that("fillByGroup with option=stop stops with multiple values", {
+    expect_error(fillByGroup(df, multiple = "stop"))
+})
+
+test_that("fillByGroup with option=majority fills multiple values", {
+    exp_res <- data.frame(A = c(rep("A", 2), rep("B", 4),
+                                rep("C", 3), "A", NA),
+                          B = c(rep("A", 2), rep("B", 4),
+                                rep("C", 3), rep(NA, 2)),
+                     C = c(1, 1, 2, 3, 2, 2, 4, 4, 4, 1, 5))
+
+   expect_equal(fillByGroup(df, multiple = "majority"), exp_res)
 
 })
