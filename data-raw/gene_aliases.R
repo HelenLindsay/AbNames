9a4ca24d5320dfe1c443537384f6ea71f54827f7
Helen Lindsay <helen.lindsay@chuv.ch>
2022-06-01 21:45:36 +0200

    exploring cellmarker database

--- data-raw/gene_aliases.R
+++ data-raw/gene_aliases.R
@@ -11,3 +11,46 @@
 
 # Single NCBI maps to multiple ENSEMBL, are both locations valid?
 # Do they produce the same protein?  Does Protein Ontology differentiate?
+
+
+gsubCellmarker <- function(x){
+    p1_matches <- stringr::str_extract_all(x, "\\[([^\\[]+)\\]")
+    p1_sub <- gsub(", ", "\\|", unlist(p1_matches))
+    p1_sub <- gsub("\\[|\\]", "", p1_sub)
+    p1_sub <- relist(p1_sub, p1_matches)
+
+    x <- cellmarker$geneSymbol
+    m <- gregexpr("\\[([^\\[]+)\\]", x[60:65])
+    regmatches(x[60:65], m) <- p1_sub[60:65]
+    return(x)
+}
+
+# Cellmarker
+cellmarker_loc <- "http://bio-bigdata.hrbmu.edu.cn/CellMarker/download/Human_cell_markers.txt"
+cellmarker_fname <- "~/Analyses/CITEseq_curation/data/CellMarker_human.txt"
+download.file(cellmarker_loc, destfile = cellmarker_fname)
+
+cellmarker <- readr::read_delim(cellmarker_fname) %>%
+    dplyr::select(cellName, cellMarker, geneSymbol,
+                  geneID, proteinName, proteinID) %>%
+    dplyr::filter(if_all(c(geneID, proteinID), ~! is.na(.x))) %>%
+    unique() %>%
+    # Separate protein complexes with | instead of [a, b, c]
+    dplyr::mutate(across(c(geneSymbol, geneID, proteinName, proteinID),
+                         ~ gsubCellmarker(.x))) %>%
+    dplyr::mutate(across(c(cellMarker, geneSymbol, geneID,
+                         proteinName, proteinID), ~strsplit(.x, ", ")))
+
+# cellMarkers match genes, genes can also have multi genes for one marker
+
+
+# Check that these are 1-to-1
+all(lengths(cellmarker$geneSymbol) == lengths(cellmarker$cellMarker))
+
+# Row Epithelial cell starting Adhesion molecules -
+# Number of cell markers is not equal to number of gene (groups),
+# is there an extra , Adhesion molecules, LFA1, Adhesion molecules LFA2?
+
+
+ #dplyr::rename(ENTREZ_ID = geneID,
+ #             UNIPROT_ID = proteinID) %>%
--- data-raw/hgnc.R
+++ data-raw/hgnc.R
@@ -1,7 +1,9 @@
 # Download HGNC data ----
 # Download gene groups and protein-coding genes tables from
-# Human Genome Naming Consortium.  As at 21.04.2022, the groups
+# Human Genome Naming Consortium.  As at 01.06.2022, the groups
 # table is not a subset of the protein-coding genes table.
+
+# Example of a multi-subunit protein - LFA-1 from ITGAL and ITGB2
 
 # HGNC proteins
 hgnc_proteins_fname <- paste(c("http://ftp.ebi.ac.uk/pub/databases/",
@@ -22,7 +24,7 @@ download.file(hgnc_groups_fname, destfile = hgnc_groups)
 hgnc_proteins <- readr::read_delim(hgnc_proteins)
 
 hgnc_proteins <- hgnc_proteins[, c("hgnc_id", "symbol", "name", "alias_symbol",
-                                   "prev_symbol", "ensembl_gene_id", "location",
+                                   "prev_symbol", "ensembl_gene_id",
                                    "alias_name", "prev_name", "uniprot_ids")]
 
 hgnc_proteins <- dplyr::rename(hgnc_proteins,
@@ -33,15 +35,17 @@ hgnc_proteins <- dplyr::rename(hgnc_proteins,
                                HGNC_SYMBOL = symbol,
                                ALIAS = alias_symbol,
                                PREVIOUS_SYMBOL = prev_symbol,
-                               Chromosome = location)
-hgnc_proteins <- dplyr::mutate(hgnc_proteins,
-                               ALIAS = gsub("\\|", ", ", ALIAS),
-                               PREVIOUS_SYMBOL =
-                                   gsub("\\|", ", ", PREVIOUS_SYMBOL))
+                               ALIAS_NAME = alias_name,
+                               PREVIOUS_NAME = prev_name)
+
+#hgnc_proteins <- dplyr::mutate(hgnc_proteins,
+#                               ALIAS = gsub("\\|", ", ", ALIAS),
+#                               PREVIOUS_SYMBOL =
+#                                   gsub("\\|", ", ", PREVIOUS_SYMBOL))
 
 hgnc_groups <- readr::read_delim(hgnc_groups)
 
-hgnc_groups <- dplyr::select(hgnc_groups, -Status, -`Locus type`,
+hgnc_groups <- dplyr::select(hgnc_groups, -Status, -`Locus type`, -`Chromosome`,
                              -`NCBI Gene ID`, -`Vega gene ID`,
                              `Group ID`, -`Group name`, -`Group ID`)
 
@@ -51,11 +55,10 @@ hgnc_groups <- dplyr::rename(hgnc_groups,
                              ENSEMBL_ID = `Ensembl gene ID`,
                              HGNC_SYMBOL = `Approved symbol`,
                              ALIAS = `Alias symbols`,
-                             PREVIOUS_SYMBOL = `Previous symbols`)
+                             PREVIOUS_SYMBOL = `Previous symbols`) %>%
+    dplyr::mutate(across(c(PREVIOUS_SYMBOL, ALIAS), ~gsub(", ", "\\|", .x)))
 
-hgnc <- dplyr::full_join(hgnc_proteins, hgnc_groups) %>%
-    dplyr::rename(ALIAS_NAME = alias_name,
-                  PREVIOUS_NAME = prev_name)
+hgnc <- dplyr::full_join(hgnc_proteins, hgnc_groups)
 
 hgnc <- unique(hgnc)
 hgnc <- as.data.frame(hgnc)
@@ -66,15 +69,39 @@ usethis::use_data(hgnc, overwrite = TRUE, compress = "bzip2")
 
 alias_grep <- "^CD|[Aa]ntigen|MHC|HLA|(T[- ]cell)|(B[- ]cell)|surface|immunoglo"
 
-hgnc_long <- dplyr::select(hgnc, -Chromosome) %>%
+hgnc <- hgnc %>%
+    dplyr::mutate(HGNC_SYMBOL2 = HGNC_SYMBOL) %>%
     tidyr::pivot_longer(c("HGNC_SYMBOL",
                           "HGNC_NAME",
                           "ALIAS",
-                          "PREVIOUS_SYMBOL"),
+                          "PREVIOUS_SYMBOL",
+                          "ALIAS_NAME",
+                          "PREVIOUS_NAME"),
                         names_to = "symbol_type") %>%
     dplyr::filter(! is.na(value)) %>%
-    AbNames::splitUnnest(ab = "value", split = ", ") %>%
-    tidyr::unnest(cols = value)
+    AbNames::splitUnnest(ab = "value", split = "\\|") %>%
+    tidyr::unnest(cols = value) %>%
+    dplyr::mutate(source = "HGNC") %>%
+    dplyr::rename(HGNC_SYMBOL = HGNC_SYMBOL2) %>%
+    unique() %>%
+
+
+    # Remove entries that match the HGNC_SYMBOL
+    dplyr::filter(! (value == HGNC_SYMBOL & ! symbol_type == "HGNC_SYMBOL")) %>%
+
+    # Only keep ambiguous values if it's an official symbol
+    dplyr::group_by(value) %>%
+    dplyr::mutate(ngroups = n_distinct(HGNC_ID)) %>%
+    dplyr::filter(ngroups == 1 | symbol_type == "HGNC_SYMBOL")
+
+
+
+
+    # Put the HGNC_SYMBOL back as a separate column
+
+
+
+
 
 hgnc_long <- as.data.frame(hgnc_long)
 usethis::use_data(hgnc_long, overwrite = TRUE, compress = "bzip2")
