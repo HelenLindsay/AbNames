7709d2e6a198a59a001515cd0be44d0015e96dd1
helen <helen@hos67669.intranet.chuv>
2022-11-15 15:50:16 +0100

    minor change to showGroups

--- R/showGroups.R
+++ R/showGroups.R
@@ -131,7 +131,7 @@ print_n <- function(df, n = 20){
 
 # Find an antibody in a data.frame and return all aliases
 # e.g. abAliases(df, "value == 'CD3'")
-abAliases <- function(df, ex, by = "HGNC"){
+abAliases <- function(df, ex, by = "HGNC_ID"){
     # Switch depending on whether ex is a string or an expression
     enex <- rlang::enexpr(ex)
 
