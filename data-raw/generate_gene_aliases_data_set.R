47591f48c557c1db4a4143fa39a790cd0ce189b8
Helen Lindsay <helen@hos67669.intranet.chuv>
2023-05-16 09:50:08 +0200

    tidying gene_aliases regeneration pipeline

--- data-raw/generate_gene_aliases_data_set.R
+++ data-raw/generate_gene_aliases_data_set.R
@@ -14,11 +14,10 @@ source("data-raw/03_entrez_ensembl.R")
 
 unloadNamespace("biomaRt")
 
-#source("data-raw/04_gene_aliases.R")
-#source("data-raw/04_protein_databases.R")
+source("data-raw/04_gene_aliases.R")
+source("data-raw/05_protein_databases.R")
 
 unloadNamespace("org.Hs.eg.db")
 
-#source("data-raw/07_manual_matches.R")
-#source("data-raw/08_antibody_registry.R")
-#source("data-raw/09_check_aliases.R")
+source("data-raw/06_manual_matches.R")
+#source("data-raw/07_check_aliases.R")
